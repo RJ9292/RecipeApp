02a6906b63197fb42ba6abcebb55ca867e07c1e1
Heath Crawford Coetsee <ST10304272@vcconnect.edu.za>
2024-04-16 15:35:20 +0200

    Added array that will reset the properties of the changes

--- RecipeApp/Class/Multiplier.cs
+++ RecipeApp/Class/Multiplier.cs
@@ -11,7 +11,7 @@ namespace RecipeApp.Class
     { // handles changing the string from the user input into a double and then applying the multiplier to it. 
         public void ApplyMultiplier( List<Ingredient> ingredients, double multiplier) //
         {
-            foreach (var ingredient in ingredients)// this will iterates through each ingredient and converts the property of the quantity to a double.
+            foreach (var ingredient in ingredients)// this will iterates through each ingredient and converts the property of the quantity to a double
             {
                 ingredient.Quantity = (double.Parse(ingredient.Quantity) * multiplier).ToString();
             }
