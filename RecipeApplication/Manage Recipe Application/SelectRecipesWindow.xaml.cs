29342de389612fa5c6409b2822102a9b791ab7e3
Heath Crawford Coetsee <ST10304272@vcconnect.edu.za>
2024-06-27 12:48:28 +0200

    Pie chart

---
+++ RecipeApplication/Manage Recipe Application/SelectRecipesWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using RecipeApp.Class;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace RecipeApplication.ManageRecipeApplication
+{
+    public partial class SelectRecipesWindow : Window
+    {
+        public List<Recipe> SelectedRecipes { get; private set; }
+
+        public SelectRecipesWindow(List<Recipe> recipes)
+        {
+            InitializeComponent();
+            lstRecipes.ItemsSource = recipes;
+            SelectedRecipes = new List<Recipe>();
+        }
+
+        private void LstRecipes_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var selectedRecipe = lstRecipes.SelectedItem as Recipe;
+            if (selectedRecipe != null && !SelectedRecipes.Contains(selectedRecipe))
+            {
+                SelectedRecipes.Add(selectedRecipe);
+                MessageBox.Show($"{selectedRecipe.RecipeName} added to selection.");
+            }
+        }
+
+        private void BtnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
