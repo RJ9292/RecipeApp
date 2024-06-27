b9515010c232dcbbaa27919238fbf53d1da4c0c4
Heath Crawford Coetsee <ST10304272@vcconnect.edu.za>
2024-06-27 12:48:15 +0200

    Resets all of the values to original

---
+++ RecipeApplication/Manage Recipe Application/ResetValuesWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using RecipeApp.Class;
+using System.Windows;
+
+namespace RecipeApplication.ManageRecipeApplication
+{
+    public partial class ResetValuesWindow : Window
+    {
+        private Recipe _recipe;
+
+        public ResetValuesWindow(Recipe recipe)
+        {
+            InitializeComponent();
+            _recipe = recipe;
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            _recipe.ResetValues();
+            MessageBox.Show("Recipe values have been reset.");
+            this.Close();
+        }
+    }
+}
