e74ef3d04e945228bed64d842a0a81847b5a0789
Heath Crawford Coetsee <ST10304272@vcconnect.edu.za>
2024-06-27 12:45:59 +0200

    Allowed them to add ingredients

---
+++ RecipeApplication/Manage Recipe Application/AddIngredientWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using RecipeApp.Class;
+
+namespace RecipeApplication.ManageRecipeApplication
+{
+    public partial class AddIngredientWindow : Window
+    {
+        private Recipe recipe;
+
+        public AddIngredientWindow()
+        {
+            InitializeComponent();
+        }
+
+        public AddIngredientWindow(Recipe recipe)
+        {
+            InitializeComponent();
+            this.recipe = recipe;
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtIngredientName.Text) &&
+                !string.IsNullOrEmpty(txtQuantity.Text) &&
+                cbMeasurementUnit.SelectedItem != null &&
+                cbFoodGroup.SelectedItem != null &&
+                int.TryParse(txtCalories.Text, out int calories))
+            {
+                Ingredient ingredient = new Ingredient(
+                    txtIngredientName.Text,
+                    txtQuantity.Text,
+                    ((ComboBoxItem)cbMeasurementUnit.SelectedItem).Content.ToString(),
+                    ((ComboBoxItem)cbFoodGroup.SelectedItem).Content.ToString(),
+                    calories);
+
+                recipe.Ingredients.Add(ingredient);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please enter all ingredient details.");
+            }
+        }
+    }
+}
