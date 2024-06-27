8e84cc1840189903ee5e2217f36dc6ace6c8ce9a
Heath Crawford Coetsee <ST10304272@vcconnect.edu.za>
2024-06-27 12:47:15 +0200

    Scrapped I am not using the filter anymore

---
+++ RecipeApplication/Manage Recipe Application/FilterWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using RecipeApp.Class;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RecipeApplication.ManageRecipeApplication
+{
+    public partial class FilterWindow : Window
+    {
+        private List<Recipe> allRecipes;
+        public List<Recipe> FilteredRecipes { get; private set; }
+
+        public FilterWindow(List<Recipe> recipes)
+        {
+            InitializeComponent();
+            allRecipes = recipes;
+            FilteredRecipes = new List<Recipe>();
+        }
+
+        private void BtnApplyFilters_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedFoodGroup = (cbFoodGroup.SelectedItem as ComboBoxItem)?.Content.ToString();
+            var ingredient = txtIngredient.Text.ToLower();
+            var maxCalories = int.TryParse(txtMaxCalories.Text, out int calories) ? calories : int.MaxValue;
+
+            FilteredRecipes = allRecipes.Where(r =>
+                (string.IsNullOrEmpty(selectedFoodGroup) || r.Ingredients.Any(i => i.FoodGroup == selectedFoodGroup)) &&
+                (string.IsNullOrEmpty(ingredient) || r.Ingredients.Any(i => i.Name.ToLower().Contains(ingredient))) &&
+                r.CalculateTotalCalories() <= maxCalories
+            ).ToList();
+
+            this.Close();
+        }
+    }
+}
