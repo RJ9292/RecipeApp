95191ced4fb8e17912d76142e046f7aa28a36a94
Heath Crawford Coetsee <ST10304272@vcconnect.edu.za>
2024-06-27 12:48:44 +0200

    Selection for menu

---
+++ RecipeApplication/Manage Recipe Application/SelectRecipeWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using RecipeApp.Class;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace RecipeApplication.ManageRecipeApplication
+{
+    public partial class SelectRecipeWindow : Window
+    {
+        public Recipe SelectedRecipe { get; private set; }
+
+        public SelectRecipeWindow(List<Recipe> recipes)
+        {
+            InitializeComponent();
+            lstRecipes.ItemsSource = recipes.Select(r => new
+            {
+                Recipe = r,
+                DisplayInfo = $"{r.RecipeName} - {r.Ingredients.Count} Ingredients - {r.CalculateTotalCalories()} Calories"
+            }).ToList();
+            lstRecipes.DisplayMemberPath = "DisplayInfo";
+            lstRecipes.SelectedValuePath = "Recipe";
+        }
+
+        private void BtnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstRecipes.SelectedItem != null)
+            {
+                SelectedRecipe = (lstRecipes.SelectedValue as Recipe);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please select a recipe.");
+            }
+        }
+    }
+}
